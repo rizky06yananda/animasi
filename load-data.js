d8e1b63cb3255d296766f1c5118a09e31476909a
Rizky <yanandarizky@gmail.com>
2022-12-28 15:13:02 +0700

    init

---
+++ load-data.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('pasarind-logo'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'animasi/data.json'
+})
